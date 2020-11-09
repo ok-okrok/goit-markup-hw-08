e2db2e2e64654b53f8773c42f3e20ba0e2b57d30
Orest Krokhmalnyi <kr.orest@gmail.com>
2020-11-09 22:35:49 +0200

    hw08-04

--- js/modal.js
+++ js/modal.js
@@ -12,3 +12,23 @@
       refs.modal.classList.toggle('is-hidden');
     }
   })();
+
+
+  (() => {
+    const refs = {
+      openModalBtn: document.querySelector('[data-modal-open1]'),
+      closeModalBtn: document.querySelector('[data-modal-close1]'),
+      modal: document.querySelector('[data-modal1]'),
+    };
+  
+    refs.openModalBtn.addEventListener('click', toggleModal);
+    refs.closeModalBtn.addEventListener('click', toggleModal);
+  
+    function toggleModal() {
+      refs.modal.classList.toggle('is-hidden');
+    }
+  })();
+
+
+
+
